37ddd5def294debcb509bef6bec0d214751c2ea0
Paulo <paulosacha.vm@gmail.com>
2021-03-13 22:03:28 -0300

    criação dos tres primeiros arquivos seguindo o padrão de modulos do node.js

---
+++ Node/Modulos/moduloCalculo.js
@@ -0,0 +1,16 @@
+module.exports = {
+    Soma (a, b) {
+        return a + b
+    },
+    Subtrai (a, b){
+        return a- b
+    },
+    Multiplica (a, b) {
+        return a * b
+    },
+    Divide (a, b){
+        return a/b
+    }
+}
+
+
